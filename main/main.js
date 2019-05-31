68ab74eb3ad241c414b4f75ae5f368f7fd8c4915
liadbiz <2712079998@qq.com>
2019-05-31 18:38:16 +0800

    fix second test

--- main/main.js
+++ main/main.js
@@ -1,7 +1,9 @@
 module.exports = function main(inputs) {
   distance = inputs["distance"];
   parkTime = inputs["parkTime"];
-  if(distance < 2){
+  if(distance <= 2){
     return Math.round(6 + parkTime * 0.25)
+  }else if(distance < 8){
+    return Math.round(6 + (distance - 2) * 0.8 + parkTime * 0.25)
   }
 };
